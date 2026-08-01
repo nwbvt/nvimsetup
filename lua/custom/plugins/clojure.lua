9232f1acd028f3c84749136c9df876ee3b002f66
Nick Brown <nwbrown@gmail.com>
2026-08-01 13:49:07 -0400

    Add clojure stuff

---
+++ lua/custom/plugins/clojure.lua
@@ -0,0 +1,18 @@
+-- lua/pack.lua
+local pack = vim.pack
+
+-- The Core REPL Tool
+pack.add({ 'https://github.com/Olical/conjure' })
+
+-- Essential structural editing and companion packs
+pack.add({ 'https://github.com/guns/vim-sexp' })
+pack.add({ 'https://github.com/tpope/vim-sexp-mappings-for-regular-people' })
+pack.add({ 'https://github.com/windwp/nvim-ts-autotag' })
+pack.add({ 'https://github.com/tpope/vim-repeat' })
+pack.add({ 'https://github.com/tpope/vim-surround' })
+pack.add({ 'https://github.com/tpope/vim-surround' })
+pack.add({ 'https://github.com/shuber/vim-promiscuous' })
+pack.add({ 'https://github.com/luochen1990/rainbow' })
+pack.add({ 'https://github.com/tpope/vim-dispatch' })
+pack.add({ 'https://github.com/clojure-vim/vim-jack-in' })
+pack.add({ 'https://github.com/radenling/vim-dispatch-neovim' })
