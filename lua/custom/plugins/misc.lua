316fd76cd1f5b4d590577640f810ae78fd74ad96
Nicholas Brown <nicholas.brown@ode.com>
2026-09-09 16:39:43 -0400

    Add fugitive and autopairs

--- lua/custom/plugins/misc.lua
+++ lua/custom/plugins/misc.lua
@@ -7,5 +7,13 @@ pack.add({ 'https://github.com/tpope/vim-repeat' })
 pack.add({ 'https://github.com/tpope/vim-surround' })
 pack.add({ 'https://github.com/tpope/vim-dispatch' })
 pack.add({ 'https://github.com/radenling/vim-dispatch-neovim' })
-pack.add { 'https://github.com/tpope/vim-fugitive' }
+pack.add({ 'https://github.com/tpope/vim-fugitive' })
+vim.keymap.set('n', "<leader>m", "", {desc = "Git"})
+vim.keymap.set('n', "<leader>ms", "<cmd>G status<cr>", {desc = "Git status"})
+vim.keymap.set('n', "<leader>mp", "<cmd>G add -up<cr>", {desc = "Git Add Patch"})
+vim.keymap.set('n', "<leader>ma", "<cmd>G add<cr>", {desc = "Git Add"})
+vim.keymap.set('n', "<leader>mc", "<cmd>G commit<cr>", {desc = "Git Commit"})
 
+pack.add({ 'https://github.com/windwp/nvim-autopairs' })
+
+require("nvim-autopairs").setup({})
