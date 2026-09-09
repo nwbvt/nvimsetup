601b945b9a3530384cd7f845da5b1956a27ea3c6
Nicholas Brown <nicholas.brown@ode.com>
2026-09-09 15:55:10 -0400

    Move some plugins to misc.lua

---
+++ lua/custom/plugins/misc.lua
@@ -0,0 +1,12 @@
+-- lua/pack.lua
+local pack = vim.pack
+
+pack.add({ 'https://github.com/tpope/vim-projectionist' })
+pack.add({ 'https://github.com/windwp/nvim-ts-autotag' })
+pack.add({ 'https://github.com/tpope/vim-repeat' })
+pack.add({ 'https://github.com/tpope/vim-surround' })
+pack.add({ 'https://github.com/tpope/vim-dispatch' })
+pack.add({ 'https://github.com/radenling/vim-dispatch-neovim' })
+
+pack.add({ 'https://github.com/guns/vim-sexp' })
+pack.add({ 'https://github.com/tpope/vim-sexp-mappings-for-regular-people' })
